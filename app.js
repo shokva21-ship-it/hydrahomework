c06c8cb54148a924d842ca1da37236564c5f2282
shokirjon <shokirjonsadikov60@examile.com>
2026-08-19 14:14:17 +0500

    Birinchi commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+let age = 20;
+if (age >= 18) {
+    console.log("Kirish mumkin")
+}
+else {
+    console.log("Kirish mumkinemas!")
+}
+
+let name = "Ali";
+let username = "Usmonov";
+if (name === "Ali") {
+    if (username = "Usmonov") {
+        console.log("Royhattan bor")
+    }
+    else {
+        console.log("Royhattan yoq")
+    }
+}
+else {
+    console.log("Royhattan yoq")
+}
+
+let ag = 21;
+
+let result = ag <= 18 ? "Adult" : "Minor";
+
+console.log(result)
